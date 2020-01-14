7364808ad88ab18033ee2ddfa48606a4dfc6daa3
liangjinlin <80264228@adc.com>
2020-01-14 18:00:39 +0800

    系统初始化

---
+++ qwwaq-ticket-entity/src/main/java/com/qwwaq/ticket/entity/QwwaqTicketEntityApplication.java
@@ -0,0 +1,13 @@
+package com.qwwaq.ticket.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwwaqTicketEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QwwaqTicketEntityApplication.class, args);
+    }
+
+}
