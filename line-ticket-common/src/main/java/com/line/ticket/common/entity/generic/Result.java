4926fe0ec57f91d2fa6397ab14a074497f952d83
liangjinlin <qwwaq@qq.com>
2020-03-16 17:44:28 +0800

    更新统一返回结果类Result的泛型工厂方法

--- line-ticket-common/src/main/java/com/line/ticket/common/entity/generic/Result.java
+++ line-ticket-common/src/main/java/com/line/ticket/common/entity/generic/Result.java
@@ -7,6 +7,11 @@ import lombok.ToString;
 
 import java.io.Serializable;
 
+/**
+ * 该类为服务间接口交互结果类, 仅提供工厂方法用于生成结果,ret的值建议在{@link ResultConstant}中定义。
+ *
+ * @param <T> 附带data数据的类型
+ */
 @Getter
 @ToString
 public class Result<T> implements Serializable {
@@ -24,44 +29,47 @@ public class Result<T> implements Serializable {
         this.ret = ret;
     }
 
-    public static <T> Result success() {
-        return new Result(null, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_SUCCESS_CODE);
+    private static final Result SIMPLE_SUCCESS_RESULT = new Result<>(null, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_SUCCESS_CODE);
+
+    private static final Result SIMPLE_FAIL_RESULT = new Result<>(null, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_FAIL_CODE);
+
+    public static Result success() {
+        return SIMPLE_SUCCESS_RESULT;
     }
 
-    public static <T> Result success(T data) {
-        return new Result(data, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_SUCCESS_CODE);
+    public static <T> Result<T> success(T data) {
+        return new Result<>(data, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_SUCCESS_CODE);
     }
 
-    public static <T> Result success(T data, String msg) {
-        return new Result(data, msg, ResultConstant.COMMON_SUCCESS_CODE);
+    public static <T> Result<T> success(T data, String msg) {
+        return new Result<>(data, msg, ResultConstant.COMMON_SUCCESS_CODE);
     }
 
-    public static <T> Result fail() {
-        return new Result(null, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_FAIL_CODE);
+    public static Result fail() {
+        return SIMPLE_FAIL_RESULT;
     }
 
-    public static <T> Result fail(T data) {
-        return new Result(data, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_FAIL_CODE);
+    public static <T> Result<T> fail(T data) {
+        return new Result<>(data, ResultConstant.EMPTY_MSG, ResultConstant.COMMON_FAIL_CODE);
     }
 
-    public static <T> Result fail(String msg) {
-        return new Result(null, msg, ResultConstant.COMMON_FAIL_CODE);
+    public static <T> Result<T> fail(String msg) {
+        return new Result<>(null, msg, ResultConstant.COMMON_FAIL_CODE);
     }
 
-    public static <T> Result fail(T data, @NonNull Integer ret) {
-        return new Result(data, ResultConstant.EMPTY_MSG, ret);
+    public static <T> Result<T> fail(T data, @NonNull Integer ret) {
+        return new Result<>(data, ResultConstant.EMPTY_MSG, ret);
     }
 
-    public static <T> Result fail(String msg, @NonNull Integer ret) {
-        return new Result(null, msg, ret);
+    public static <T> Result<T> fail(String msg, @NonNull Integer ret) {
+        return new Result<>(null, msg, ret);
     }
 
-
-    public static <T> Result fail(T data, String msg) {
-        return new Result(data, msg, ResultConstant.COMMON_FAIL_CODE);
+    public static <T> Result<T> fail(T data, String msg) {
+        return new Result<>(data, msg, ResultConstant.COMMON_FAIL_CODE);
     }
 
-    public static <T> Result fail(T data, String msg, @NonNull Integer ret) {
-        return new Result(data, msg, ret);
+    public static <T> Result<T> fail(T data, String msg, @NonNull Integer ret) {
+        return new Result<>(data, msg, ret);
     }
 }
